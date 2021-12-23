6d5ae77b48ff3cd4d67898cfb71f4343c7ac6555
sorokinanastya <77689176+sorokinanastya@users.noreply.github.com>
2021-12-23 15:42:55 +0300

    slider

--- portfolio/js/app.js
+++ portfolio/js/app.js
@@ -3,12 +3,12 @@ const slides = document.querySelectorAll('.slide')
 for (const slide of slides) {
   slide.addEventListener('click', () => {
     clearActiveClasses()
-    slide.classList.add('active')
+    //slide.classList.add('active')
   })
 
   function clearActiveClasses() {
     slides.forEach((slide) => {
-      slide.classList.remove('active')
+      slide.classList.remove('active', 'swipe-slide-active')
     })
   }
 }
