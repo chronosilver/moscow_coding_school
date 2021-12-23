ab3914199b5c39ad7d771d4fab04a1783561f954
sorokinanastya <77689176+sorokinanastya@users.noreply.github.com>
2021-12-23 16:12:06 +0300

    mobile-swiper

--- portfolio/js/app.js
+++ portfolio/js/app.js
@@ -1,14 +1,30 @@
 const slides = document.querySelectorAll('.slide')
+const container = document.querySelector('.container')
+const slider = document.querySelector('.slider')
+const slide = document.querySelectorAll('.slide')
 
-for (const slide of slides) {
-  slide.addEventListener('click', () => {
-    clearActiveClasses()
-    //slide.classList.add('active')
-  })
+if (!/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+  for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses()
+      slide.classList.add('active')
+    })
 
-  function clearActiveClasses() {
+    function clearActiveClasses() {
+      slides.forEach((slide) => {
+        slide.classList.remove('active')
+      })
+    }
+  }
+}
+function Add() {
+  if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+    container.classList.add('swiper-wrapper')
+    slider.classList.add('swiper', 'mySwiper')
     slides.forEach((slide) => {
-      slide.classList.remove('active', 'swipe-slide-active')
+      slide.classList.remove('active')
+      slide.classList.add('swiper-slide')
     })
   }
 }
+Add()
